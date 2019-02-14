7253b5b966dd6d1fe32aa8288ec032611e22079b
Guillaume Vagner <guillaume.vagner@supelec.fr>
2019-02-15 00:09:41 +0100

    je sais pas vraiment

---
+++ mongoose.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/botday')
+  .then(() =>  console.log('[mongoose] Connection succesful'))
+  .catch((err) => console.error(err));
+
+module.exports = mongoose;
