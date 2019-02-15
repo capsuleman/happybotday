068289856e3f13b96c2f87adc22582c809ff6c4e
Guillaume Vagner <guillaume.vagner@supelec.fr>
2019-02-15 18:41:29 +0100

    ajouté

--- index.js
+++ index.js
@@ -1,2 +1,40 @@
 const bot = require('./telegram');
 const app = require('./website');
+const mongoose = require('./mongoose');
+const Token = require('./models/Token');
+
+const rp = require('request-promise');
+
+function sendRequest(req, token, callback) {
+    const options = {
+        headers: { 'Authorization': `Bearer ${token}` },
+        json: true
+    }
+    const url = 'https://gateway.linkcs.fr/v1/graphql';
+
+
+    return rp(`${url}?query=${req}`, options)
+}
+
+
+function getBirthdays(token) {
+    req = 'query getUsersBirthday {users: usersBirthday {    ...userData}}fragment userData on User {id  firstName  lastName  roles {sector {composition {association {id}}}}}'
+    return sendRequest(req, token).then(body => {
+        const users = [];
+        body.data.users.forEach(user => {
+            use = {};
+            use.name = `${user.firstName} ${user.lastName}`;
+            use.asso = [];
+            user.roles.forEach(role => {
+                use.asso.push(role.sector.composition.association.id);
+            })
+            users.push(use);
+        });
+        return users;
+    })
+
+}
+
+// Token.findOne({ expiration: { $gt: Date.now() } }).then(token => {
+//     getBirthdays(token.token).then(a => console.log(a));
+// })
--- website.js
+++ website.js
@@ -3,18 +3,25 @@ const config = require('./config');
 const app = require('express')();
 const request = require('request');
 const bot = require('./telegram');
+const Token = require('./models/Token');
 
 app.listen(80, config.website.hostname, () => {
     console.log(`[express] Website is up and accessible on ${config.website.protocol}://${config.website.hostname}/`);
 })
 
 app.get('/', function (req, res) {
-    if (!req.query.chatid) { return res.sendFile(`${__dirname}/index.html`) };
 
-    const state = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-    const redirectURI = config.website.protocol + '://' + config.website.hostname + '/auth';
-    const url = `https://auth.viarezo.fr/oauth/authorize/?redirect_uri=${redirectURI}&client_id=${config.oauth2.clientid}&response_type=code&state=${state}&scope=${config.oauth2.scope}`;
-    return res.redirect(301, url);
+    if (!req.query.state) { return res.sendFile(`${__dirname}/index.html`) };
+
+    Token.findOne({ state: req.query.state }).then(rep => {
+
+        if (!rep || rep.token) return res.sendFile(`${__dirname}/index.html`);
+
+        const redirectURI = `${config.website.protocol}://${config.website.hostname}/auth`;
+        const url = `https://auth.viarezo.fr/oauth/authorize/?redirect_uri=${redirectURI}&client_id=${config.oauth2.clientid}&response_type=code&state=${rep.state}&scope=${config.oauth2.scope}`;
+        return res.redirect(301, url);
+    })
+
 })
 
 app.get('/background.jpg', function (req, res) {
@@ -22,7 +29,7 @@ app.get('/background.jpg', function (req, res) {
 })
 
 app.get('/auth', function (req, res) {
-    if (!req.query.code || !req.query.state) { return res.sendFile(`${__dirname}/auth.html`) }
+    if (!req.query.code || !req.query.state) return res.sendFile(`${__dirname}/auth.html`)
 
     const options = {
         url: 'https://auth.viarezo.fr/oauth/token',
@@ -38,9 +45,17 @@ app.get('/auth', function (req, res) {
     }
 
     request(options, (err, res, body) => {
-        console.log(res.statusCode);
         if (!err && res.statusCode == 200) {
-            console.log(body)
+            Token.findOne({ state: req.query.state }).then(token => {
+                rep = JSON.parse(body)
+                if (!token) { return req.query.state }
+                token.token = rep.access_token;
+                token.expiration = rep.expires_at*1000;
+                token.state = '';
+                return token.save();
+            }).then(token => {
+                bot.sendMessage(token.chatId, `@${token.username} s'est connecté à OAuth2, shall we begin?`)
+            })
         }
     })
     return res.redirect(301, `${config.website.protocol}://${config.website.hostname}/auth`);
