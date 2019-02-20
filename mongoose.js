e479f8bcafb1ad6c6198e7eab6dc13e3695ea629
Guillaume Vagner <guillaume.vagner@supelec.fr>
2019-02-21 00:49:43 +0100

    changed to mysql

--- mongoose.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/botday', { useNewUrlParser: true })
-    .then(() => console.log('[mongoose] Connection succesful'))
-    .catch((err) => console.error(err));
-
-module.exports = mongoose;
